93203696497fcdc1c90e1958d783a67804831a30
Shehanka <hcsperera@outlook.com>
2019-11-05 00:28:34 +0530

    mongo db connected

--- api/server.js
+++ api/server.js
@@ -4,6 +4,19 @@ const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+const config = require('./DB.jsx');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {
+    console.log('DB is connected !!');
+  },
+  err => {
+    console.log('Cannot connect to DB ' + err);
+  }
+);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
