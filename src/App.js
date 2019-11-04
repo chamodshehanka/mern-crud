053653eabd53e073998c0b2cecb206b7ee9bc462
Shehanka <hcsperera@outlook.com>
2019-11-04 22:33:55 +0530

    Router used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import './App.css';
-// import ''
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className='container'>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <Link to={'/'} className='navbar-brand'>
+            MERN CRUD
+          </Link>
+
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav mr-auto'>
+              <li className='nav-item'>
+                <Link to={'/'} className='nav-link'>
+                  Home
+                </Link>
+              </li>
+
+              <li className='nav-item'>
+                <Link to={'/create'} className='nav-link'>
+                  Create
+                </Link>
+              </li>
+
+              <li className='nav-item'>
+                <Link to={'/index'} className='nav-link'>
+                  Index
+                </Link>
+              </li>
+
+              <li className='nav-item'>
+                <Link to={'/edit'} className='nav-link'>
+                  Edit
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br></br>
+        <h2>Welcome to MERN CRUD</h2>
+
+        <Switch>
+          <Route exact path='/create' component={Create} />
+          <Route exact path='/edit:id' component={Edit} />
+          <Route exact path='/index' component={Index} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
